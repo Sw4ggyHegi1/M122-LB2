5b9d4ae634503a3fed34d9e3c7cb74ae37078704
DaddyHegii <78270367+DaddyHegii@users.noreply.github.com>
2023-11-22 11:36:19 +0100

    Update EzAD.ps1
    - Mehr User Input möglich
    - CSV Import möglich

--- EzAD.ps1
+++ EzAD.ps1
@@ -1,46 +1,83 @@
-﻿param(
-    [string]$UserName,
+param (
+    [string]$AddUser,
     [string]$DeleteUser,
-    [string]$Vorname,
-    [string]$Nachname,
-    [string]$Passwort
+    [string]$Import,
+    [string]$Search
 )
 
+# Überprüft, ob ein Benutzer erstellt werden soll
+if ($AddUser) {
+    $UserName = Read-Host "Geben Sie den SamAccountName des Benutzers ein"
 
-# --- User erstellen ---
-# Prüft ob user schon vorhanden ist
-if (Get-ADUser -Filter {SamAccountName -eq $UserName}) {
-    Write-Host "Benutzer $UserName existiert bereits."
-    Exit
+    # Prüft, ob der Benutzer bereits vorhanden ist
+    if (Get-ADUser -Filter { SamAccountName -eq $UserName }) {
+        Write-Host "Benutzer $UserName existiert bereits."
+    } else {
+        $UserParams = @{}
+        
+        $UserParams.SamAccountName = $UserName
+        $UserParams.UserPrincipalName = "$($UserParams.SamAccountName)@m122.ch"
+        $UserParams.Name = Read-Host "Geben Sie den Namen des Benutzers ein (Vorname Nachname)"
+        $NameParts = $UserParams.Name -split " "
+        $UserParams.GivenName = $NameParts[0]
+        $UserParams.Surname = $NameParts[1]
+        $UserParams.Enabled = $true
 
-} else {
-    $UserParams = @{
-        SamAccountName = $UserName
-        UserPrincipalName = "$UserName@m122.ch"
-        Name = "$Vorname $Nachname"
-        GivenName = $Vorname
-        Surname = $Nachname
-        Enabled = $true
-        AccountPassword = (ConvertTo-SecureString -AsPlainText $Passwort -Force)
+        $Password = Read-Host "Geben Sie das Passwort des Benutzers ein" -AsSecureString
+        $UserParams.AccountPassword = $Password
+
+        New-ADUser @UserParams
+        Write-Host "Benutzer $($UserParams.SamAccountName) wurde erfolgreich erstellt."
     }
 
-    New-ADUser @UserParams
-    Write-Host " "
-    Write-Host "Benutzer $UserName wurde erfolgreich erstellt."
-    Write-Host " "
-    Exit
-}
-# --- User erstellen ende ---
+} elseif ($DeleteUser) {
+    # Prüft, ob der zu löschende Benutzer existiert
+    if (Get-ADUser -Filter { SamAccountName -eq $DeleteUser }) {
+        Remove-ADUser -Identity $DeleteUser -Confirm:$false
+        Write-Host "Benutzer $DeleteUser wurde erfolgreich gelöscht."
+    } else {
+        Write-Host "Benutzer $DeleteUser existiert nicht."
+    }
+    
+} elseif ($Import) {
+    $csvPath = Read-Host "Geben Sie den Pfad zur CSV-Datei für den Massenimport ein"
 
-# --- User löschen ---
-# Löscht Benutzer
-if (Get-ADUser -Filter {SamAccountName -eq $DeleteUser}) {
-    Remove-ADUser -Identity $DeleteUser -Confirm:$false
-    Write-Host "Benutzer $DeleteUser wurde erfolgreich gelöscht."
+    if (-not (Test-Path -Path $csvPath -PathType Leaf)) {
+        Write-Host "Die angegebene Datei existiert nicht oder ist ungültig. Bitte geben Sie einen gültigen Pfad an."
+    } else {
+        # Einlesen der CSV-Datei
+        $users = Import-Csv -Path $csvPath
 
-# Falls user nicht existiert
+        # Durchlaufen der Benutzerdaten und Erstellen der Benutzer im AD
+        foreach ($user in $users) {
+            $firstName = $user.Vorname
+            $lastName = $user.Nachname
+            $userName = $user.Benutzername
+            $password = $user.Passwort
+            $department = $user.Abteilung
+
+            # Prüfen, ob der Benutzer bereits vorhanden ist
+            if (-not (Get-ADUser -Filter { SamAccountName -eq $userName })) {
+                # Benutzer erstellen
+                New-ADUser -SamAccountName $userName -GivenName $firstName -Surname $lastName -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -Enabled $true -Department $department
+                Write-Host "Benutzer '$userName' wurde erstellt."
+            } else {
+                Write-Host "Benutzer '$userName' existiert bereits."
+            }
+        }
+    }
+
+# --- UNDER CONSTRUCTION ---
+} elseif ($Search) {
+    $UserName = ""
+    
+    if (Get-ADUser -Filter { SamAccountName -eq $UserName}){
+        Write-Host "Benutzer '$userName' existiert."
+    } else {
+        Write-Host "Benutzer '$userName' existiert nicht."
+    }
+# --- UNDER CONSTRUCTION ---
+
 } else {
-    Write-Host "Benutzer $DeleteUser existiert nicht."
+    Write-Host "Es wurde keine gültige Aktion angegeben. Verwenden Sie '-AddUser', '-DeleteUser' oder '-Import', um eine Aktion auszuführen."
 }
-
-# --- User löschen ende ---
